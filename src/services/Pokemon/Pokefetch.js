3ca6df9e5047fc36daeb128f9127cf3b2873bf16
Joshua Stresing <pyrotorch69@yahoo.com>
2022-03-25 14:20:47 -0700

    update for net

--- src/services/Pokemon/Pokefetch.js
+++ src/services/Pokemon/Pokefetch.js
@@ -5,6 +5,5 @@ export async function fetchPokemon() {
   const resp = await fetch (`https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}&perPage=10`);
   const data = await resp.json();
 
-  console.log(data);
   return data.results;
 }
