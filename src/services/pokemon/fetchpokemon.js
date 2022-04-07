51859479edc58116440bba67f156350b336410bd
Joshua Stresing <pyrotorch69@yahoo.com>
2022-04-07 14:56:19 -0700

    removed logs

--- src/services/pokemon/fetchpokemon.js
+++ src/services/pokemon/fetchpokemon.js
@@ -4,14 +4,12 @@ export async function fetchPokemon() {
 
   const resp = await fetch (`https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}&perPage=10`);
   const data = await resp.json();
-  console.log(data);
   return data.results;
 }
 
 export async function fetchTypes() {
   const resp = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex/types');
   const data = await resp.json();
-  console.log(data);
   return data.map((type) => type.type);
 }
 
